d68916e6cbf63d026726575ba81c872c8dee438e
Pascal T. Hein <pascal.hein@gmx.de>
2020-05-19 16:41:45 +0200

    reformatted for scalability

--- app.R
+++ app.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(DramaAnalysis)
-library(hash)
+
+avaliableDramas <- list("Emilia Galotti" = "rksp.0", "Miß Sara Sampson" = "rjmw.0")
 
 ui <- fluidPage(
   
@@ -11,29 +12,42 @@ ui <- fluidPage(
   sidebarLayout(
     # Sidebar panel for inputs ----
     sidebarPanel(
-      selectInput(inputId="text", "Text auswählen:", choices=list("Emilia Galotti" = "rksp.0", "Miß Sara Sampson" = "rjmw.0"))
+      selectInput(inputId="dramaID", "Text auswählen:", choices=avaliableDramas)
     ),
     # Main panel for displaying outputs ----
     mainPanel(
-      # Output: Histogram ----
-      plotOutput(outputId = "plot")
+      tabsetPanel(
+        tabPanel("Utterance Quantity", plotOutput("quant")),
+        tabPanel("Utterance Distribution", plotOutput("dist")),
+        tabPanel("Character Presence", plotOutput("presence")),
+        tabPanel("Copresence", plotOutput("copresence"))
+      )
     )
   )
 )
 
 
 server <- function(input, output) {
-  # loads a specific, pre-packaged play
-  data(rksp.0)
-  data(rjmw.0)
-  charStatsEmil <- characterStatistics(rksp.0)
-  charStatsSara <- characterStatistics(rjmw.0)
-  charStatsEmil <- characterNames(charStatsEmil, rksp.0)
-  charStatsSara <- characterNames(charStatsSara, rjmw.0)
-  dataDict = hash("rksp.0"=charStatsEmil, "rjmw.0"=charStatsSara)
+  dramas <- loadDrama(avaliableDramas, defaultCollection ="test")
   
+  thisDrama <- reactive({
+    drama <- list("text" = dramas$text[dramas$text$drama == input$dramaID],
+                  "meta" = dramas$meta[dramas$meta$drama == input$dramaID],
+                  "segments" = dramas$segments[dramas$segments$drama == input$dramaID],
+                  "mentions" = dramas$mentions[dramas$mentions$drama == input$dramaID],
+                  "characters" = dramas$characters[dramas$characters$drama == input$dramaID],
+                  "stageDirections" = dramas$stageDirections[dramas$stageDirections$drama == input$dramaID])
+    class(drama) <- c("QDDrama", "list")
+    drama
+  }) 
+
+  charStats <- reactive({
+    characterNames(characterStatistics(thisDrama()), thisDrama())
+  })
+
   # plot them as a bar plot
-  output$plot <- renderPlot(barplot(dataDict[[input$text]], main=input$text))
+  output$quant <- renderPlot(barplot(charStats(), main=dramaNames(thisDrama())))
+  
 }
 
 shinyApp(ui, server)
