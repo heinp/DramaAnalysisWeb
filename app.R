1e816f3b6c7c4690b96c8644606feb72cf605bf4
Pascal T. Hein <pascal.hein@gmx.de>
2020-06-08 14:55:15 +0200

    slider for each task, better DramaNames

--- app.R
+++ app.R
@@ -1,7 +1,17 @@
 library(shiny)
 library(DramaAnalysis)
 
-avaliableDramas <- list("Emilia Galotti" = "rksp.0", "Miß Sara Sampson" = "rjmw.0")
+tryCatch(installData(dataSource="qd"), error = function(e) {})
+
+ids <- loadAllInstalledIds()
+ids <- ids[which(grepl("qd:", ids))]
+metaData <- loadMeta(ids)
+#avaliableDramas <- as.character(metaData$drama)
+#names(avaliableDramas) <- lapply(avaliableDramas, function(x) dramaNames(x))
+
+avaliableDramas <- setNames(as.character(metaData$drama), paste(metaData$Name, ": ", metaData$documentTitle, " (", metaData$Date.Written, ")", sep=""))
+
+
 
 ui <- fluidPage(
   
@@ -12,12 +22,13 @@ ui <- fluidPage(
   sidebarLayout(
     # Sidebar panel for inputs ----
     sidebarPanel(
-      selectizeInput(inputId="dramaID", "Select a drama:", choices=avaliableDramas, selected=NULL, multiple=FALSE, options=list(create=TRUE, placeholder="Select a drama"))
+      selectizeInput(inputId="dramaID", "Select a drama:", choices=avaliableDramas, selected=NULL, multiple=FALSE, options=list(create=TRUE, placeholder="Select a drama")),
+      uiOutput("intSlider")
     ),
     # Main panel for displaying outputs ----
     mainPanel(
       tabsetPanel(
-        tabPanel("Utterance Quantity", plotOutput("quant"), uiOutput("intSlider")),
+        tabPanel("Utterance Quantity", plotOutput("quant")),
         tabPanel("Utterance Distribution", plotOutput("dist", width=500)),
         tabPanel("Character Presence", plotOutput("presence", width=500)),
         tabPanel("Copresence", plotOutput("copresence", width=500, height=500))
@@ -29,58 +40,66 @@ ui <- fluidPage(
 
 
 server <- function(input, output) {
-  dramas <- loadDrama(avaliableDramas, defaultCollection ="test")
-  
   # get the selected Drama
   thisDrama <- reactive({
     validate(
       need(input$dramaID != "", "Please select drama")
     )
-    drama <- list("text" = dramas$text[dramas$text$drama == input$dramaID],
-                  "meta" = dramas$meta[dramas$meta$drama == input$dramaID],
-                  "segments" = dramas$segments[dramas$segments$drama == input$dramaID],
-                  "mentions" = dramas$mentions[dramas$mentions$drama == input$dramaID],
-                  "characters" = dramas$characters[dramas$characters$drama == input$dramaID],
-                  "stageDirections" = dramas$stageDirections[dramas$stageDirections$drama == input$dramaID])
-    class(drama) <- c("QDDrama", "list")
-    drama
+
+    loadDrama(input$dramaID)
   })
   
-  # create utterance quantity stats
+  # create slider for choice of top N
+  output$intSlider <- renderUI(sliderInput("topN", "Only show top N characters:", min=2, max=nrow(charStats()), value=nrow(charStats()), step=1))
+  
+  
+  # create sorted charecter stats (for utterance quantity and for topN)
   charStats <- reactive({
     stats <- characterNames(characterStatistics(thisDrama()), thisDrama())
     stats <- stats[order(-stats$tokens),]
+    stats
   })
   
-  topNStats <- reactive({
-    tryCatch(head(charStats(), as.integer(input$maxNAbs)), error = function(e) charStats())
+  # get topN figures
+  topNFigs <- reactive({
+    tryCatch(head(charStats()$character, input$topN), error=function(e) charStats()$character)
+  })
+  
+  
+  topNCharStats <- reactive({
+    charStats()[charStats()$character %in% topNFigs(),]
   })
 
   # plot utterance quantity as a bar plot
-  output$quant <- renderPlot(barplot(topNStats(), main=dramaNames(thisDrama())), width=500)
-  output$intSlider <- renderUI(sliderInput("maxNAbs", "Only show top N characters:", min=2, max=nrow(charStats()), value=nrow(charStats()), step=1))
+  output$quant <- renderPlot(barplot(topNCharStats(), main=dramaNames(thisDrama())), width=500)
   
   #create utterance distribution stats
   
   uttStats <- reactive({
     stats <- utteranceStatistics(thisDrama())
     stats <- characterNames(stats, thisDrama())
+    stats
+  })
+  
+  topNUttStats <- reactive({
+    uttStats()[uttStats()$character %in% topNFigs(), ]
   })
   
   # plot utterance distribution
   uttDistPlot <- reactive({
     par(mar=c(2,9,2,2))
-    plot(uttStats(), thisDrama(), main=dramaNames(thisDrama()))
+    drama <- lapply(thisDrama(), function(x) x[x$character %in% topNFigs(),])
+    class(drama) <- c("QDDrama", "list") 
+    plot(topNUttStats(), drama, main=dramaNames(thisDrama()))
   })
   
-  # output$dist <- renderPlot(plot(uttStats(), thisDrama(), main=dramaNames(thisDrama())), width=500)
-  output$dist <- renderCachedPlot(uttDistPlot(), {input$dramaID})
+  output$dist <- renderCachedPlot(uttDistPlot(),  c(input$dramaID, input$topN))
   
   
   # create copresence stats
   co <- reactive({
     co <- configuration(thisDrama(), onlyPresence = TRUE, segment="Scene")
-    co <- filterCharacters(co, thisDrama(), n = 7)
+    co <- filterCharacters(co, thisDrama(), n=input$topN)
     characterNames(co, thisDrama())
   })
   
@@ -123,7 +142,7 @@ server <- function(input, output) {
     # add the y axis
     axis(2, at = seq(0,1,length.out = length(co()$character)), labels = co()$character, las=1)
   })
-  output$copresence <- renderCachedPlot(heat(), {input$dramaID})
+  output$copresence <- renderCachedPlot(heat(),  c(input$dramaID, input$topN))
   
   
   # create character presence stats
@@ -133,24 +152,25 @@ server <- function(input, output) {
   
   # plot character presence stats
   presplot <- reactive({
-    plot(x=pres()$active/pres()$scenes, 
-         y=pres()$passive/pres()$scenes, 
+    pres <- pres()[pres()$character %in% topNFigs(), ]
+    plot(x=pres$active/pres$scenes, 
+         y=pres$passive/pres$scenes, 
          xlim=c(0,1), 
          ylim=c(0,1), 
          xlab="Active", 
          ylab="Passive",
          sub=dramaNames(thisDrama()),
          main="Character Presence")
-    text(x=pres()$actives/pres()$scenes, 
-         y=pres()$passives/pres()$scenes, 
-         labels=substr(pres()$character,0,20), 
+    text(x=pres$actives/pres$scenes, 
+         y=pres$passives/pres$scenes, 
+         labels=substr(pres$character,0,20), 
          pos=3,
          cex=0.8)
     lines(x=seq(0,0.5,0.1),seq(0,0.5,0.1), lty=3)
     lines(x=1:0,y=0:1, lty=2)
   })
   
-  output$presence <- renderCachedPlot(presplot(), {input$dramaID})
+  output$presence <- renderCachedPlot(presplot(), c(input$dramaID, input$topN))
   
 }
 
